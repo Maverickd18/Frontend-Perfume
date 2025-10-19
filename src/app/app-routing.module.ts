7d17053d9f784c1cb2809508feca1403a7e7d56b
Levelasquez15 <lewis.velasquezwatts@unicolombo.edu.co>
2025-10-19 18:25:02 -0500

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
-    path: 'profile-seller',
-    loadChildren: () => import('./pages/profile-seller/profile-seller.module').then(m => m.ProfileSellerPageModule)
-  },  {
-    path: 'forgot-password',
-    loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
-  }
-=======
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { StoreGuard } from './guards/store.guard';
@@ -54,9 +24,12 @@ const routes: Routes = [
     path: 'profile-seller',
     loadChildren: () => import('./pages/profile-seller/profile-seller.module').then(m => m.ProfileSellerPageModule),
     canActivate: [StoreGuard]
-  }
-
-  ,{
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./pages/forgot-password/forgot-password.module').then(m => m.ForgotPasswordPageModule)
+  },
+  {
     path: 'seller',
     loadChildren: () => import('./pages/seller/seller.module').then(m => m.SellerPageModule)
   },
@@ -65,10 +38,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/notifications/notifications.module').then(m => m.NotificationsPageModule),
     canActivate: [StoreGuard]
   }
-
-];
->>>>>>> seller
-
 ];
 
 @NgModule({
