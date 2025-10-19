4d01e6c7351bd1ce912eb62ffe021f1ad55bac8a
Owen Badel Hooker <owenbadel19@gmail.com>
2025-10-18 21:31:49 -0500

    add navigation methods for header actions
    methods goBack, logout and goHome

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular'; // 👈 necesario para navegación
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,56 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  constructor(private navCtrl: NavController) {}
 
-  constructor() {}
+  products = [
+    {
+      id: 1,
+      title: 'Rosa Suave',
+      brand: 'Maverick',
+      description: 'Aromatic floral with soft musk notes.',
+      image: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      price: 29.99
+    },
+    {
+      id: 2,
+      title: 'Noche Ambar',
+      brand: 'Maverick',
+      description: 'Warm, amber and vanilla tones for evening wear.',
+      image: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      price: 39.99
+    },
+    {
+      id: 3,
+      title: 'Citrus Fresh',
+      brand: 'Maverick',
+      description: 'Bright citrus with green top notes, perfect for daytime.',
+      image: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      price: 24.99
+    }
+  ];
 
+  // 👉 Métodos de botones del header
+  goBack() {
+    this.navCtrl.back();
+  }
+
+  logout() {
+    alert('Sesión cerrada');
+    // luego puedes limpiar el storage y redirigir al login
+    this.navCtrl.navigateRoot('/login');
+  }
+
+  goHome() {
+    this.navCtrl.navigateRoot('/home');
+  }
+
+  // 👉 Métodos de las cards
+  onView(p: any) {
+    alert('Ver producto: ' + p.title);
+  }
+
+  onBuy(p: any) {
+    alert('Comprar producto: ' + p.title + '\nPrecio: $' + p.price);
+  }
 }
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   standalone: false,
@@ -11,5 +11,22 @@ export class CardComponent  implements OnInit {
   constructor() { }
 
   ngOnInit() {}
+
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() description: string = '';
+  @Input() image: string = '';
+  @Input() price: number | null = null;
+
+  @Output() viewProduct = new EventEmitter<void>();
+  @Output() buyProduct = new EventEmitter<void>();
+
+  view() {
+    this.viewProduct.emit();
+  }
+
+  buy() {
+    this.buyProduct.emit();
+  }
 
 }
